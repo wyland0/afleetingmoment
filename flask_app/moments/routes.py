3112660081f020190f9c229da69ba18e924e12b4
jinson-j <jinson.jimmy05@gmail.com>
2025-04-24 17:29:27 -0400

    comments page and whatever

--- flask_app/moments/routes.py
+++ flask_app/moments/routes.py
@@ -8,10 +8,8 @@ moments = Blueprint("moments", __name__)
 
 @moments.route("/")
 def index():
-
     moments = []
-
-    # dummies
+    # dummies just use all when needed
     for i in range(10):
         moments.append({
             "id": i,
@@ -19,3 +17,29 @@ def index():
         })
 
     return render_template("index.html", key=google_client.getKey(), moments=moments)
+
+@moments.route("/createmoment", methods=["POST", "GET"])
+def create_moment():
+
+    if request.method == "GET":
+        return render_template("createmoment.html")
+    
+    
+@moments.route("/comment", methods=["GET"])
+def create_comment():
+    id = request.args.get('id')
+
+    comments = []
+    # dummies just use all when needed
+    for i in range(10):
+        comments.append({
+            "id": i,
+            "username": f"User {i}",
+            "content": f"Commwadcgjm,l;kmjhngtdrzedscv hbuy7t6edxcghyu7tredghyuent {i}",
+            "time": f"2023-10-01 12:00:00",
+        })
+    return render_template("createcomment.html", id=id, comments=comments)
+
+@moments.route("/comment/<id>", methods=["POST"])
+def post_comment():
+    return render_template("createcomment.html", id=id)
