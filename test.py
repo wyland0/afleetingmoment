e325807e78183e8793f2025b91596dd5a825e063
Wyland On <123030919+wyland0@users.noreply.github.com>
2025-04-24 16:50:55 -0400

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import googlemaps
-
-gmaps = googlemaps.Client(key='AIzaSyDTSXje9oiQUb1CzLcq6t48Y0FYRicsWys')
-geocode_result = gmaps.geocode('UMD')
-
-print(geocode_result[0]['geometry']['location'])
